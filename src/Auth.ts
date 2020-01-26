93f6129935a7533f0b19c6d3d86e7db55a58ca46
Ben Johnson <weagle08@gmail.com>
2020-01-25 22:10:11 -0600

    published

--- src/Auth.ts
+++ src/Auth.ts
@@ -1,17 +1,54 @@
+import axios, { AxiosResponse } from 'axios';
+import { Constants } from './Constants';
+import { IDto } from './IDto';
+import { ITokenResponse } from './ITokenResponse';
+import { IUserProfile } from './IUserProfile';
+
 export class Auth {
-    public static exchangeCodeForToken() {
+    public static async exchangeCodeForToken(code: string, clientId: number, clientSecret: string): Promise<string> {
+        let url = `${Constants.AUTH_BASE_URL}?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`;
 
+        let response = await axios.get<IDto<ITokenResponse>, AxiosResponse<IDto<ITokenResponse>>>(url);
+
+        if (response.status === 200 && response.data != null && response.data.data != null && response.data.data.token != null) {
+            return response.data.data.token;
+        } else {
+            throw new Error('token request failed');
+        }
     }
 
-    public static getUserProfile() {
+    public static async getUserProfile(token: string): Promise<IUserProfile> {
+        let url = `${Constants.AUTH_BASE_URL}/api/users/me`;
+        let response = await axios.get<IDto<IUserProfile>, AxiosResponse<IDto<IUserProfile>>>(url, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
 
+        if (response.status === 200 && response.data != null && response.data.data != null) {
+            return response.data.data;
+        }
+
+        throw new Error('user profile request failed');
     }
 
-    public validateToken() {
+    public async validateToken(token: string): Promise<boolean> {
+        let url = `${Constants.AUTH_BASE_URL}/api/auth/token?token=${token}`;
 
+        let response = await axios.get<IDto<boolean>, AxiosResponse<IDto<boolean>>>(url);
+        if (response.status === 200 && response.data != null && response.data.data != null) {
+            return response.data.data;
+        }
+
+        return false;
     }
 
-    public refreshToken() {
+    public async refreshToken(token: string, clientId: number, clientSecret: string): Promise<string> {
+        let url = `${Constants.AUTH_BASE_URL}/api/auth/token?token=${token}&client_id=${clientId}&client_secret=${clientSecret}`;
 
+        let response = await axios.post<IDto<boolean>, AxiosResponse<IDto<ITokenResponse>>>(url);
+        if (response.status === 200 && response.data != null && response.data.data != null) {
+            return response.data.data.token;
+        }
     }
 }
---
+++ src/Constants.ts
@@ -0,0 +1,3 @@
+export const Constants = {
+    AUTH_BASE_URL: 'http://auth.sameke.com'
+};
